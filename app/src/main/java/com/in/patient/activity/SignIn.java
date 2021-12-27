ca180d7e1d20606196e64ee7631ab8aee0a598d0
Mehul888 <mehulrathodit@gmail.com>
2021-12-27 19:40:21 +0530

    commit

--- app/src/main/java/com/in/patient/activity/SignIn.java
+++ app/src/main/java/com/in/patient/activity/SignIn.java
@@ -3,10 +3,23 @@ package com.in.patient.activity;
 import static com.in.patient.globle.Glob.Token;
 import static com.in.patient.globle.Glob.user_id;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +32,21 @@ import com.in.patient.model.SignInModel;
 import com.in.patient.retrofit.Api;
 import com.in.patient.retrofit.RetrofitClient;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,6 +57,12 @@ public class SignIn extends AppCompatActivity {
     EditText edtEmail, edtPassword;
 
 
+    private KeyStore keyStore;
+    private static final String KEY_NAME = "androidHive";
+    private Cipher cipher;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +70,10 @@ public class SignIn extends AppCompatActivity {
         getSupportActionBar().hide();
 
         init();
+        accessFingerprint();
+
     }
+
 
     public void init() {
 
@@ -91,5 +128,166 @@ public class SignIn extends AppCompatActivity {
 
     }
 
+
+    @TargetApi(Build.VERSION_CODES.M)
+    protected void generateKey() {
+        try {
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        KeyGenerator keyGenerator;
+        try {
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException("Failed to get KeyGenerator instance", e);
+        }
+
+
+        try {
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException |
+                InvalidAlgorithmParameterException
+                | CertificateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void accessFingerprint() {
+        KeyguardManager keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        FingerprintManager fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+
+        if (!fingerprintManager.isHardwareDetected()) {
+            /**
+             * An error message will be displayed if the device does not contain the fingerprint hardware.
+             * However if you plan to implement a default authentication method,
+             * you can redirect the user to a default authentication activity from here.
+             * Example:
+             * Intent intent = new Intent(this, DefaultAuthenticationActivity.class);
+             * startActivity(intent);
+             */
+            Toast.makeText(getApplicationContext(), "Your Device does not have a Fingerprint Sensor", Toast.LENGTH_SHORT).show();
+
+        } else {
+            // Checks whether fingerprint permission is set on manifest
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Fingerprint authentication permission not enabled", Toast.LENGTH_SHORT).show();
+            } else {
+                // Check whether at least one fingerprint is registered
+                if (!fingerprintManager.hasEnrolledFingerprints()) {
+                    Toast.makeText(getApplicationContext(), "Register at least one fingerprint in Settings", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // Checks whether lock screen security is enabled or not
+                    if (!keyguardManager.isKeyguardSecure()) {
+                        Toast.makeText(getApplicationContext(), "Lock screen security not enabled in Settings", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        generateKey();
+
+
+                        if (cipherInit()) {
+                            FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                            FingerprintHandler helper = new FingerprintHandler(this);
+                            helper.startAuth(fingerprintManager, cryptoObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+
+
+        private Context context;
+
+
+        // Constructor
+        public FingerprintHandler(Context mContext) {
+            context = mContext;
+        }
+
+
+        public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
+            CancellationSignal cancellationSignal = new CancellationSignal();
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+        }
+
+
+        @Override
+        public void onAuthenticationError(int errMsgId, CharSequence errString) {
+            this.update("Fingerprint Authentication error\n" + errString, false);
+        }
+
+
+        @Override
+        public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+            this.update("Fingerprint Authentication help\n" + helpString, false);
+        }
+
+
+        @Override
+        public void onAuthenticationFailed() {
+            this.update("Fingerprint Authentication failed.", false);
+        }
+
+
+        @Override
+        public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+            this.update("Fingerprint Authentication succeeded.", true);
+        }
+
+
+        public void update(String e, Boolean success) {
+//            TextView textView = (TextView) ((Activity)context).findViewById(R.id.errorText);
+//            textView.setText(e);
+            if (success) {
+//                textView.setTextColor(ContextCompat.getColor(context,R.color.colorPrimaryDark));
+                Toast.makeText(getApplicationContext(), "Sucess", Toast.LENGTH_SHORT).show();
+            } else
+                Toast.makeText(getApplicationContext(), "" + e, Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
